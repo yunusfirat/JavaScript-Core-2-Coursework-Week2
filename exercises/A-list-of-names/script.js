d651c073f44bbce8119434696775271a5c056e64
yunusfirat <yunusfirat44@gmail.com>
2021-02-23 23:37:52 +0000

    list of names

--- exercises/A-list-of-names/script.js
+++ exercises/A-list-of-names/script.js
@@ -1,5 +1,13 @@
-function listOfNames(arrayOfPeople) {
+function listOfNames(People) {
   let content = document.querySelector("#content");
+  People.forEach(person => {
+    let nameTag = document.createElement('h1')
+    let jobTag = document.createElement('h2')
+    nameTag.innerHTML = person.name
+    jobTag.innerHTML = person.job
+    content.appendChild(nameTag)
+    content.appendChild(jobTag)
+  })
 }
 
 let people = [
