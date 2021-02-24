43e4e11d10ea9e7fabcc884bc02f794e00adfc34
yunusfirat <yunusfirat44@gmail.com>
2021-02-24 21:29:21 +0000

    shopping-list

--- exercises/B-shopping-list/script.js
+++ exercises/B-shopping-list/script.js
@@ -1,5 +1,14 @@
 function shoppingList(arrayOfPeople) {
   // Write your code here...
+  let content = document.querySelector('#content')
+  arrayOfPeople.forEach(person => {
+    let ultag = document.createElement('ul');
+    let litag = document.createElement('li');
+    litag.innerHTML = person;
+    let ullitag = ultag.appendChild(litag);
+    content.appendChild(ullitag);
+    console.log(person);
+  });
 }
 
 let shopping = ["Milk", "Bread", "Eggs", "A Dinosaur", "Cake", "Sugar", "Tea"];
