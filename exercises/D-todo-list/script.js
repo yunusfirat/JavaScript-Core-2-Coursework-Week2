46d1a3449cd689eca74691919f3423450428d77d
yunusfirat <yunusfirat44@gmail.com>
2021-02-26 00:57:06 +0000

    D-todo-list

--- exercises/D-todo-list/script.js
+++ exercises/D-todo-list/script.js
@@ -1,5 +1,27 @@
 function todoList(todos) {
   // Write your code here...
+  let content = document.querySelector('#content');
+  todos.forEach(todo => {
+    let ultag = document.createElement('ul');
+    let litag = document.createElement('li');
+    litag.style.listStyle = "none";
+    litag.style.textDecoration = "line-through"
+    content.appendChild(ultag).appendChild(litag);
+    litag.innerHTML = todo.todo;
+    
+  })
+  const listItem =document.querySelectorAll("li");
+  console.log(listItem);
+    listItem.forEach(list => {
+      list.addEventListener("click", () => {
+        if(list.style.textDecoration === "line-through"){
+          list.style.textDecoration = "none"
+        }else{
+          list.style.textDecoration = "line-through"
+        }
+    });
+  });
+
 }
 
 const todos = [
